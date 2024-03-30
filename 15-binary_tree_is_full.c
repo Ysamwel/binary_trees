163db46b784ebf096ecb2abb5022e00f77b533ca
Ysamwel <yvonneatieno2000@gmail.com>
2024-03-30 04:23:08 +0300

    full-tree

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -6,11 +6,12 @@
  */
 
 int binary_tree_is_full(const binary_tree_t *tree)
+int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
-	if (tree->right != NULL && tree->left != NULL)
+	if (!tree->right && !tree->left)
 		return (1);
 
 	if (tree->right && tree->left)
